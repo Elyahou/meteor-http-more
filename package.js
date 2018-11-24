a5323044b8884ba599e48a6db2a914e1862d3618
Elyahou Ittah <elyahou.i@fiverr.com>
2018-11-24 23:38:21 +0200

    update request version

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   git: 'https://github.com/dandv/meteor-http-more.git'
 });
 
-Npm.depends({request: "2.33.0"});
+Npm.depends({request: "2.88.1"});
 
 Package.onUse(function (api) {
   api.versionsFrom('0.9.0');
